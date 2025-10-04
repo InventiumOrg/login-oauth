05941be1950e662a89c9a8ca25cca7affafc4804
nylespham <nylespham@gmail.com>
2025-10-04 21:24:17 +0700

    small change

--- main.go
+++ main.go
@@ -1,120 +1,120 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"login-oauth/config"
-	"login-oauth/controllers"
-	"login-oauth/routes"
-	"login-oauth/services"
-	"net/http"
+  "context"
+  "fmt"
+  "log"
+  "login-oauth/config"
+  "login-oauth/controllers"
+  "login-oauth/routes"
+  "login-oauth/services"
+  "net/http"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis/v8"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+  "github.com/gin-contrib/cors"
+  "github.com/gin-gonic/gin"
+  "github.com/go-redis/redis/v8"
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
+  "go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var (
-	server      *gin.Engine
-	ctx         context.Context
-	mongoclient *mongo.Client
-	redisclient *redis.Client
+  server      *gin.Engine
+  ctx         context.Context
+  mongoclient *mongo.Client
+  redisclient *redis.Client
 
-	userService         services.UserService
-	UserController      controllers.UserController
-	UserRouteController routes.UserRouteController
+  userService         services.UserService
+  UserController      controllers.UserController
+  UserRouteController routes.UserRouteController
 
-	authCollection      *mongo.Collection
-	authService         services.AuthService
-	AuthController      controllers.AuthController
-	AuthRouteController routes.AuthRouteController
+  authCollection      *mongo.Collection
+  authService         services.AuthService
+  AuthController      controllers.AuthController
+  AuthRouteController routes.AuthRouteController
 )
 
 func init() {
-	config, err := config.LoadConfig(".")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
+  config, err := config.LoadConfig(".")
+  if err != nil {
+    log.Fatal("cannot load config:", err)
+  }
 
-	ctx := context.TODO()
+  ctx := context.TODO()
 
-	// Connect to MongoDB
+  // Connect to MongoDB
+  // mongoconn := options.Client().ApplyURI(config.DBUri)
+  mongoconn := options.Client().ApplyURI("mongodb+srv://nylespham:Strator@202501@inventium.qpgk4db.mongodb.net/?retryWrites=true&w=majority&appName=Inventium")
+  mongoClient, err := mongo.Connect(ctx, mongoconn)
 
-	mongoconn := options.Client().ApplyURI(config.DBUri)
-	mongoClient, err := mongo.Connect(ctx, mongoconn)
+  if err != nil {
+    panic(err)
+  }
 
-	if err != nil {
-		panic(err)
-	}
+  if err := mongoClient.Ping(ctx, readpref.Primary()); err != nil {
+    panic(err)
+  }
 
-	if err := mongoClient.Ping(ctx, readpref.Primary()); err != nil {
-		panic(err)
-	}
+  fmt.Println("Connected to MongoDB!")
 
-	fmt.Println("Connected to MongoDB!")
+  // Connect to Redis
+  redisclient = redis.NewClient(&redis.Options{
+    Addr: config.RedisUri,
+  })
 
-	// Connect to Redis
-	redisclient = redis.NewClient(&redis.Options{
-		Addr: config.RedisUri,
-	})
+  if _, err := redisclient.Ping(ctx).Result(); err != nil {
+    panic(err)
+  }
 
-	if _, err := redisclient.Ping(ctx).Result(); err != nil {
-		panic(err)
-	}
+  err = redisclient.Set(ctx, "test", "Welcome to Golang with Redis and MongoDB", 0).Err()
+  if err != nil {
+    panic(err)
+  }
 
-	err = redisclient.Set(ctx, "test", "Welcome to Golang with Redis and MongoDB", 0).Err()
-	if err != nil {
-		panic(err)
-	}
+  fmt.Println("Redis client connected successfully...")
+  // Collections
+  authCollection = mongoclient.Database("golang_mongodb").Collection("users")
+  userService = services.NewUserServiceImpl(authCollection, ctx)
+  authService = services.NewAuthService(authCollection, ctx)
+  AuthController = controllers.NewAuthController(authService, userService)
+  AuthRouteController = routes.NewAuthRouteController(AuthController)
 
-	fmt.Println("Redis client connected successfully...")
-	// Collections
-	authCollection = mongoclient.Database("golang_mongodb").Collection("users")
-	userService = services.NewUserServiceImpl(authCollection, ctx)
-	authService = services.NewAuthService(authCollection, ctx)
-	AuthController = controllers.NewAuthController(authService, userService)
-	AuthRouteController = routes.NewAuthRouteController(AuthController)
+  UserController = controllers.NewUserController(userService)
+  UserRouteController = routes.NewRouteUserController(UserController)
 
-	UserController = controllers.NewUserController(userService)
-	UserRouteController = routes.NewRouteUserController(UserController)
+  server = gin.Default()
 
-	server = gin.Default()
+  }
 
-}
+  func main() {
+  config, err := config.LoadConfig(".")
 
-func main() {
-	config, err := config.LoadConfig(".")
+  if err != nil {
+    log.Fatal("Could not load config", err)
+  }
 
-	if err != nil {
-		log.Fatal("Could not load config", err)
-	}
+  defer mongoclient.Disconnect(ctx)
 
-	defer mongoclient.Disconnect(ctx)
+  value, err := redisclient.Get(ctx, "test").Result()
 
-	value, err := redisclient.Get(ctx, "test").Result()
+  if err == redis.Nil {
+    fmt.Println("key: test does not exist")
+  } else if err != nil {
+    panic(err)
+  }
 
-	if err == redis.Nil {
-		fmt.Println("key: test does not exist")
-	} else if err != nil {
-		panic(err)
-	}
+  corsConfig := cors.DefaultConfig()
+  corsConfig.AllowOrigins = []string{"http://localhost:8000", "http://localhost:3000"}
+  corsConfig.AllowCredentials = true
 
-	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowOrigins = []string{"http://localhost:8000", "http://localhost:3000"}
-	corsConfig.AllowCredentials = true
+  server.Use(cors.New(corsConfig))
 
-	server.Use(cors.New(corsConfig))
+  router := server.Group("/api")
+  router.GET("/healthchecker", func(ctx *gin.Context) {
+    ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
+  })
 
-	router := server.Group("/api")
-	router.GET("/healthchecker", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
-	})
-
-	AuthRouteController.AuthRoute(router, userService)
-	UserRouteController.UserRoute(router, userService)
-	log.Fatal(server.Run(":" + config.Port))
+  AuthRouteController.AuthRoute(router, userService)
+  UserRouteController.UserRoute(router, userService)
+  log.Fatal(server.Run(":" + config.Port))
 }
